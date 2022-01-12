f9db2ac7f9a7eb33dfcf11472ead1cfe7e9b2efa
Sanjay R <sanjayprashanth1997@gmail.com>
2022-01-12 11:38:47 +0530

    port changed

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import dotenv from "dotenv"
 dotenv.config();
 console.log(process.env)
 const app = express();
-const PORT = 9000; 
+const PORT = process.env.PORT; 
 const movies = [
     {
     "id": "104",
